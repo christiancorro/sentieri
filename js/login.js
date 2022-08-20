e01f75bd4e9c1afc48c93efcdff4eaa83a651cce
Christian Corrò <christian.corro@outlook.com>
2022-08-20 18:33:30 +0200

    ref

--- js/login.js
+++ js/login.js
@@ -81,7 +81,7 @@ function LoggedOut() {
 function returnToIndex() {
     setTimeout(() => {
         if (window.location.pathname != "index.html" && window.location.pathname != "/" && window.location.pathname != "/sentieri/") {
-            document.location.href = "/sentieri";
+            document.location.href = "/sentieri/";
         }
     }, 400);
 
